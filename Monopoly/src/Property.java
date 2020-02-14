911d6a804fda613434bb6e52a5a175e10f7b0dbe
hiltonluu <hiltonluu@10.13.81.57>
2020-02-14 14:07:39 -0700

    made getters and setters

--- Monopoly/src/Property.java
+++ Monopoly/src/Property.java
@@ -1,17 +1,25 @@
-
+/*
+ * If you add variables, make getters and setter plz
+ * 
+ * 
+ * 
+ * 
+ * */
 public class Property {
-	private int price;
-	private Player owner;
 	
-
-	public Player getOwner() {
+	//initializing variables
+	private int price, color, numOfHouses, numOfHotels, positionOnBoard, rentBase, rent1House, rent2House, rent3Hourse, rent4House, rentHotel, mortgageValue, houseCost, hotelCost;
+	private String owner, name;
+	
+//Getters and Setters
+	public String getOwner() {
 		return owner;
 	}
 
-	public void setOwner(Player owner) {
+	public void setOwner(String owner) {
 		this.owner = owner;
 	}
-
+	
 	public int getPrice() {
 		return price;
 	}
@@ -19,4 +27,117 @@ public class Property {
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int color) {
+		this.color = color;
+	}
+
+	public int getNumOfHouses() {
+		return numOfHouses;
+	}
+
+	public void setNumOfHouses(int numOfHouses) {
+		this.numOfHouses = numOfHouses;
+	}
+
+	public int getNumOfHotels() {
+		return numOfHotels;
+	}
+
+	public void setNumOfHotels(int numOfHotels) {
+		this.numOfHotels = numOfHotels;
+	}
+
+	public int getPositionOnBoard() {
+		return positionOnBoard;
+	}
+
+	public void setPositionOnBoard(int positionOnBoard) {
+		this.positionOnBoard = positionOnBoard;
+	}
+
+	public int getRentBase() {
+		return rentBase;
+	}
+
+	public void setRentBase(int rentBase) {
+		this.rentBase = rentBase;
+	}
+
+	public int getRent1House() {
+		return rent1House;
+	}
+
+	public void setRent1House(int rent1House) {
+		this.rent1House = rent1House;
+	}
+
+	public int getRent2House() {
+		return rent2House;
+	}
+
+	public void setRent2House(int rent2House) {
+		this.rent2House = rent2House;
+	}
+
+	public int getRent3Hourse() {
+		return rent3Hourse;
+	}
+
+	public void setRent3Hourse(int rent3Hourse) {
+		this.rent3Hourse = rent3Hourse;
+	}
+
+	public int getRent4House() {
+		return rent4House;
+	}
+
+	public void setRent4House(int rent4House) {
+		this.rent4House = rent4House;
+	}
+
+	public int getRentHotel() {
+		return rentHotel;
+	}
+
+	public void setRentHotel(int rentHotel) {
+		this.rentHotel = rentHotel;
+	}
+
+	public int getMortgageValue() {
+		return mortgageValue;
+	}
+
+	public void setMortgageValue(int mortgageValue) {
+		this.mortgageValue = mortgageValue;
+	}
+
+	public int getHouseCost() {
+		return houseCost;
+	}
+
+	public void setHouseCost(int houseCost) {
+		this.houseCost = houseCost;
+	}
+
+	public int getHotelCost() {
+		return hotelCost;
+	}
+
+	public void setHotelCost(int hotelCost) {
+		this.hotelCost = hotelCost;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
