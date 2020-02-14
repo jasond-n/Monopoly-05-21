27b6613d210cde31cb57cb1c1a96aed32e340e81
KaiOoOKai <kaihuang0804@gmail.com>
2020-02-14 13:38:13 -0700

    update Player class and Property class

--- Monopoly/src/Player.java
+++ Monopoly/src/Player.java
@@ -5,15 +5,36 @@ public class Player {
 	private String avatar;
 	private ArrayList<Property> properties;
 	private int balance;
-	private Boolean Injail;
+	private Boolean injail;
 	
 	public Player(String avatar, int balance)
 	{
 		this.avatar = avatar;
 		this.balance = balance;
 		this.properties = new ArrayList<Property>();
+		this.injail = false;
 	}
 	
+	public void sellProperty(Property theProperty)
+	{
+		theProperty.setOwner(null);
+		this.properties.remove(theProperty);
+		this.balance = this.balance + theProperty.getPrice();
+	}
 	
+	public Boolean buyProperty(Property theProperty)
+	{
+		if(this.balance < theProperty.getPrice())
+		{
+			return false;
+		}
+		else
+		{
+			theProperty.setOwner(this);
+			this.properties.add(theProperty);
+			this.balance = this.balance - theProperty.getPrice();
+			return true;
+		}
+	}
 	
 }
--- Monopoly/src/Property.java
+++ Monopoly/src/Property.java
@@ -1,4 +1,22 @@
 
 public class Property {
+	private int price;
+	private Player owner;
+	
 
+	public Player getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Player owner) {
+		this.owner = owner;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
 }
