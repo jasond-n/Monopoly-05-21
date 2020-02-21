5a7550e6c11ed21eb40b05f4926b1dad6b1b40df
KaiOoOKai <kaihuang0804@gmail.com>
2020-02-21 14:23:36 -0700

    Create Utility.java

---
+++ Monopoly/src/Utility.java
@@ -0,0 +1,8 @@
+
+public class Utility extends Property {
+
+	public Utility(int price, int positionOnBoard, int rentBase, int rent1House, int mortgageValue, String owner, String name)
+	{
+		super(price, positionOnBoard, rentBase, rent1House, mortgageValue, owner, name);
+	}
+}
