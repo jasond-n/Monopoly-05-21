c34c3c736919d3b2071c9aa46525a270aa5272d1
justinjelly <justinjelly@gmail.com>
2020-02-21 14:42:25 -0700

    Update Board.java

--- Monopoly/src/Board.java
+++ Monopoly/src/Board.java
@@ -72,21 +72,57 @@ public class Board {
 
 	public Board () { //in the constructor possibly add number of players?
 
-		Card money1 = new Card("You have just recieved your student loans. Collect $50.", "money", 50);
-		Card money2 = new Card("A bus just ran you over on campus. Collect $150 from the lawsuit settlement.", "money", 150);
-		Card money3 = new Card("Pay $15 for a pizza bun at Bake Chef.", "money", -15);
-		Card money4 = new Card("You got a scholarship for maintaining a 4.0 GPA last semester. Collect $100", "money", 100);
-		Card move1 = new Card("MATH 265 has no more open lecture seats left this semester. Move back 3 spaces.","move", -3);
-		Card move2 = new Card("Advance to GO. Collect $200.", "move", 1);
+		// coding all the event cards to go in community chest and chance decks
+		Card money20 = new Card("You sold an old textbook. Collect $20.", "money", 20);
+		Card money50 = new Card("You have just recieved your student loans. Collect $50.", "money", 50);
+		Card money100a = new Card("You earned a scholarship for maintaining a 4.0 GPA last semester. Collect $100", "money", 100);
+		Card money100b = new Card("You helped an international student with an assignemt. In return, he gave you $100.", "money", 100);
+		Card money150 = new Card("You won first place in a city-wide hackathon. Collect $150.", "money", 150);
+		Card money200 = new Card("A bus just ran you over on campus. Collect $200 from the lawsuit settlement.", "money", 200);
+		Card moneyN15 = new Card("Pay $15 for a pizza bun at Bake Chef.", "money", -15);
+		Card moneyN50a = new Card("Pay a $50 fine for dropping out of astronomy 207. Yep, way too much physics.", "money", -50);
+		Card moneyN50b = new Card("You fell asleep during your final exam and got a D. Cramming was definitely a bad idea. Lose $50.", "money", -50);
+		Card moveN3 = new Card("MATH 265 has no more open lecture seats left this semester. Move back 3 spaces.","move", -3);
+		Card moveTo1 = new Card("Advance to GO. Collect $200.", "move", 1);
 		// in move2, value = 1 represents the index for the GO space. use 41 - player.getLocation() instead? the number of spaces player needs to go forwards to get to GO. (player get location is needed)	
-		Card move3 = new Card("Advance to Science Theatres. If you pass GO, collect $200", "move", 25);
-		Card move4 = new Card("Advance to Reeve Theatre. If you pass GO, collect $200.", "move", 12);
-		Card move5 = new Card("Take a trip to Somerset Station. If you pass Go, collect $200", "move", 6);
+		Card moveTo25 = new Card("Advance to Science Theatres. If you pass GO, collect $200", "move", 25);
+		Card moveTo12 = new Card("Advance to Reeve Theatre. If you pass GO, collect $200.", "move", 12);
+		Card moveTo40 = new Card("Take a trip to the Taylor Institute. If you pass GO, collect $200.", "move", 40);
+		Card moveTo6 = new Card("Take a trip to Somerset Station. If you pass Go, collect $200", "move", 6);
 		// advance to nearest utility. If owned, throw dice and pay owner a total 10 times the amount thrown.
 		// Advance to nearest Railroad and pay owner twice the rental amount.
 		// Get out of Jail Free. This card may be kept until needed, or traded/sold.
 		// Make general repairs on all your property: For each house pay $25, For each hotel pay $100.
-		// You have been elected as an SU faculty representative. Pay each player $50.
+		// You have been elected as SU president. Pay each player $50.
+		// You are hosting a networking night for software engineers. Each player pays you a $50 entrance fee.
+		// You got the highest grade on the midterm in your class. It was 57. Collect $10 from each player.
+		
+		
+		// creating the chance and community chest decks as ArrayLists and adding the appropriate cards
+		ArrayList<Card> chanceDeck = new ArrayList<Card>();
+		chanceDeck.add(money50);
+		chanceDeck.add(money150);
+		chanceDeck.add(moneyN15);
+		chanceDeck.add(money100a);
+		chanceDeck.add(moveN3);
+		chanceDeck.add(moveTo1);
+		chanceDeck.add(moveTo25);
+		chanceDeck.add(moveTo12);
+		chanceDeck.add(moveTo6);
+		chanceDeck.add(moveTo40);
+		// add nearest utility, nearest railroad, get out of jail, go to jail, repairs, SU president
+		ArrayList<Card> communityDeck = new ArrayList<Card>();
+		communityDeck.add(money200);
+		communityDeck.add(money20);
+		communityDeck.add(moneyN50a);
+		communityDeck.add(moneyN50b);
+		communityDeck.add(money100a);
+		communityDeck.add(money150);
+		communityDeck.add(money100b);
+		communityDeck.add(money50);
+		communityDeck.add(moveTo1);
+		communityDeck.add(moveTo40);
+		// add go to jail, get out of jail, network night, midterm, repairs
 		
 		allSpotsOnBoard = new ArrayList<Property>();
 		//in the constructor possibly add number of players?
