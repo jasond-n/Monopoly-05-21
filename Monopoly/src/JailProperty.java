8b8aab514d438f0f30d140c5cbd3f2f23dac993d
KaiOoOKai <kaihuang0804@gmail.com>
2020-02-21 13:52:50 -0700

    Create all types of properties

--- Monopoly/src/JailProperty.java
+++ Monopoly/src/JailProperty.java
@@ -1,5 +1,5 @@
 
-public class SpecialProperty extends Property {
+public class JailProperty extends Property {
 	
 	public SpecialProperty() {
 		super(price, positionOnBoard, rentBase, rent2, rent3, rent4);
