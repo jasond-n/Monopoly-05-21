cfa401bff1e5188fce27a2a279b7ba3b6956a8fc
KaiOoOKai <kaihuang0804@gmail.com>
2020-02-21 14:18:47 -0700

    Update Chance.java

--- Monopoly/src/ChanceProperty.java
+++
@@ -1,6 +0,0 @@
-public class ChanceProperty extends Property {
-	public ChanceProperty(int price, int positionOnBoard, int rentBase, int rent1House, int rent2House, int rent3House, int mortgageValue, String owner, String name)
-	{
-		super(price, positionOnBoard, rentBase, rent1House, rent2House, rent3House, mortgageValue, owner, name);
-	}
-}
