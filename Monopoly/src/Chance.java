69198f594e6da7acfeba60017cd7b14d7541a508
KaiOoOKai <kaihuang0804@gmail.com>
2020-02-21 14:20:29 -0700

    Update Chance.java

--- Monopoly/src/Chance.java
+++ Monopoly/src/Chance.java
@@ -1,6 +1,6 @@
 public class Chance extends Property {
-	public Chance(int price, int positionOnBoard, int rentBase, int rent1House, int rent2House, int rent3House, int mortgageValue, String owner, String name)
+	public Chance(int positionOnBoard, String name)
 	{
-		super(price, positionOnBoard, rentBase, rent1House, rent2House, rent3House, mortgageValue, owner, name);
+		super(positionOnBoard, name);
 	}
 }
