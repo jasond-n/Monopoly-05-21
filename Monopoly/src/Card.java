646d1cde70ddddfebce3e8e651c7c2d3dd113a55
justinjelly <justinjelly@gmail.com>
2020-02-22 11:02:53 -0700

    Update Card.java

--- Monopoly/src/Card.java
+++ Monopoly/src/Card.java
@@ -19,20 +19,11 @@ public class Card {
 	public String getDesc() {
 		return desc;
 	}
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
 	public String getType() {
 		return type;
 	}
-	public void setType(String type) {
-		this.type = type;
-	}
 	public int getValue() {
 		return value;
-	}
-	public void setValue(int value) {
-		this.value = value;
 	}
 	
 	// does this go in card class?
