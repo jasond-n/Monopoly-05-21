b02d16297cd41bf51eee4bb5fce442a6a5047f55
Jason Nguyen <51870695+jasond-n@users.noreply.github.com>
2020-02-23 17:48:21 -0700

    Update RailroadProperty.java

--- Monopoly/src/RailroadProperty.java
+++ Monopoly/src/RailroadProperty.java
@@ -5,10 +5,9 @@ public class RailroadProperty extends Property {
 		super(price, positionOnBoard, rentBase, rent1House, rent2House, rent3House, mortgageValue, owner, name);
 	}
 	
-	public void doActionAfterPlayerLandingHere(Player player, int roll, Board board)
-	{
+	public void doActionAfterPlayerLandingHere(Player player, int roll, Board board) {
 		String userInput;
-		
+		// buy property meaning no owner
 		if(this.getOwner() == null)
 		{
 			// write the code to ask the player "Do you want to buy this railroad?
@@ -23,6 +22,39 @@ public class RailroadProperty extends Property {
 					}
 				}
 			//sc.close();
+		}
+		//has owner. Player must buy rent to owner
+		else if (board.getProperties().get(getPositionOnBoard()).getOwner() != player && board.getProperties().get(getPositionOnBoard()).getOwner() != null) {
+		System.out.println("You have to pay the owner of the property!");
+		if (getNumOfHotels() == 0) {
+			switch(getNumOfHouses()) {
+			case 0: 
+				player.loseMoney(getRentBase());
+				board.getProperties().get(getPositionOnBoard()).getOwner().addMoney(getRentBase());
+				break;
+			case 1: 
+				player.loseMoney(getRent1House());
+				board.getProperties().get(getPositionOnBoard()).getOwner().addMoney(getRent1House());
+				break;
+			case 2: 
+				player.loseMoney(getRent2House());
+				board.getProperties().get(getPositionOnBoard()).getOwner().addMoney(getRent2House());
+				break;
+			case 3: 
+				player.loseMoney(getRent3House());
+				board.getProperties().get(getPositionOnBoard()).getOwner().addMoney(getRent3House());
+				break; 
+			case 4: 
+				player.loseMoney(getRent4House());
+				board.getProperties().get(getPositionOnBoard()).getOwner().addMoney(getRent4House());
+				break;
+				}
+			}
+		
+		if (getNumOfHotels() == 1) {
+			player.loseMoney(getRentHotel());
+			board.getProperties().get(getPositionOnBoard()).getOwner().addMoney(getRentHotel());
+			}
 		}
 	}
 }
