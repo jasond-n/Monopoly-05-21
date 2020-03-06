e7c9298bae691cd37974b3c046c1c14946b16b4d
Jason Nguyen <51870695+jasond-n@users.noreply.github.com>
2020-03-06 13:06:42 -0700

    JavaFX

---
+++ Monopoly/src/GameController.java
@@ -0,0 +1,27 @@
+/**
+ * Sample Skeleton for 'Monopoly.fxml' Controller Class
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class GameController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML
+    void diceroll(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
+}
---
+++ Monopoly/src/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Monopoly.fxml"));
+		
+		Scene scene = new Scene(root);
+		
+		stage.setScene(scene);
+		stage.setTitle("My birthday!");
+		stage.show();
+	}
+}
