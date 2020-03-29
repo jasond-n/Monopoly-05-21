9fc2311376fe7c08523cfac5d5a527925bb6b498
Jason Nguyen <51870695+jasond-n@users.noreply.github.com>
2020-03-28 22:04:44 -0600

    Update MainMenuController.java

--- Monopoly/src/MainMenuController.java
+++ Monopoly/src/MainMenuController.java
@@ -6,17 +6,14 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -108,8 +105,8 @@ public class MainMenuController extends Board {
 		}
     	try {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/Monopoly.fxml"));
-			GameController gameController = fxmlLoader.getController();
-	        gameController.transferMessage(String.valueOf( getPlayerCount()) );
+			//GameController gameController = fxmlLoader.getController();
+	        //gameController.transferMessage(String.valueOf(getPlayerCount()));
 	            
 			Parent root = (Parent) fxmlLoader.load();
 			
